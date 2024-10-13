acfa38a7c183a23442a1662e951528ead7971b2f
Ariel Flores <132795526+Ariel-FloresS@users.noreply.github.com>
2024-10-12 20:36:06 -0600

    Add files via upload

---
+++ autores.py
@@ -0,0 +1,58 @@
+composer_files = {
+    'Beethoven': [
+        'beethoven_hammerklavier_2.mid',
+        'beethoven_les_adieux_2.mid',
+        'beethoven_opus10_2.mid',
+        'beethoven_opus22_2.mid',
+        'beethoven_opus22_3.mid',
+        'waldstein_2.mid',
+    ],
+    'Brahms': [
+        'brahms_opus117_1.mid',
+        'brahms_opus117_2.mid',
+        'brahms_opus1_2.mid',
+    ],
+    'Chopin': [
+        'chpn-p1.mid', 'chpn-p2.mid', 'chpn-p3.mid', 'chpn-p4.mid', 'chpn-p5.mid',
+        'chpn-p6.mid', 'chpn-p7.mid', 'chpn-p8.mid', 'chpn-p9.mid', 'chpn-p10.mid',
+        'chpn-p11.mid', 'chpn-p12.mid', 'chpn-p13.mid', 'chpn-p14.mid', 'chpn-p15.mid',
+        'chpn-p16.mid', 'chpn-p17.mid', 'chpn-p18.mid', 'chpn-p19.mid', 'chpn-p20.mid',
+        'chpn-p21.mid', 'chpn-p22.mid', 'chpn-p23.mid', 'chpn-p24.mid',
+        'chpn_op10_e01.mid', 'chpn_op10_e05.mid', 'chpn_op10_e12.mid',
+        'chpn_op25_e2.mid', 'chpn_op25_e3.mid', 'chpn_op25_e4.mid', 'chpn_op27_1.mid',
+        'chpn_op7_1 (1).mid', 'chpn_op7_2.mid'
+    ],
+    'Debussy': [
+        'debussy_cc_1.mid', 'debussy_cc_2.mid', 'debussy_cc_3.mid',
+        'debussy_cc_4.mid', 'debussy_cc_5.mid', 'debussy_cc_6.mid',
+        'deb_clai.mid', 'deb_menu.mid', 'deb_pass.mid', 'deb_prel.mid'
+    ],
+    'Grieg': [
+        'grieg_album.mid', 'grieg_berceuse.mid', 'grieg_brooklet.mid', 'grieg_butterfly.mid',
+        'grieg_elfentanz.mid', 'grieg_halling.mid', 'grieg_kobold.mid', 'grieg_march.mid',
+        'grieg_once_upon_a_time.mid', 'grieg_spring.mid', 'grieg_voeglein.mid', 
+        'grieg_waechter.mid', 'grieg_walzer.mid', 'grieg_wanderer.mid', 
+        'grieg_zwerge.mid'
+    ],
+    'Liszt': [
+        'liz_et4.mid', 'liz_et5.mid', 'liz_liebestraum.mid'
+    ],
+    'Mozart': [
+        'mz_311_2.mid', 'mz_330_2.mid', 'mz_332_2.mid', 
+        'mz_545_2.mid', 'mz_545_3.mid', 'mz_570_2.mid', 'mz_570_3.mid'
+    ],
+    'Tchaikovsky': [
+        'ty_april.mid', 'ty_februar.mid', 'ty_januar.mid', 'ty_juli.mid',
+        'ty_juni.mid', 'ty_maerz.mid', 'ty_mai.mid', 'ty_november.mid', 
+        'ty_oktober.mid', 'ty_september.mid'
+    ],
+    'Rachmaninoff': [
+        'pathetique_2.mid'
+    ],
+    'Schumann': [
+        'schumm-2.mid', 'schumm-3.mid', 'schumm-5.mid', 'schumm-6.mid'
+    ],
+    'Strauss': [
+        'br_im2.mid', 'br_im5.mid', 'br_im6.mid'
+    ]
+}
---
+++ streamlit_app.py
@@ -0,0 +1,95 @@
+import streamlit as st
+import pandas as pd
+import os
+from src.lstm_model import load_lstm
+from src.seed import genera_semilla
+from src.prediccion import generar_notas
+from src.midi_file import dataframe_a_midi, midi_to_wav
+from autores import composer_files
+
+# Reconocimiento al autor
+st.markdown("""
+        ---
+        **Página y modelo creados por**: Ariel Flores Santacruz
+        """)
+st.title("Generador de Música con LSTM 🎶")
+
+# Contenedor para la explicación
+with st.container():
+    st.header("Generación de Música de Autores Clásicos ✨")
+    st.write("""
+        Bienvenido a mi aplicación, donde generaremos música basada en compositores clásicos como 
+        Beethoven, Debussy, entre otros. Utilizamos una **Red Neuronal de Memoria a Largo Plazo (LSTM)**, que es un 
+        tipo de red neuronal recurrente (RNN) diseñada para aprender de secuencias temporales.
+    """)
+
+    st.subheader("❓ ¿Qué es una LSTM?")
+    st.write("""
+        Las LSTM (Long Short-Term Memory) son un tipo de red neuronal diseñada para recordar información a 
+        largo plazo y gestionar secuencias de datos, utilizando puertas para decidir qué información conservar 
+        o eliminar, lo que las hace ideales para aplicaciones como la generación de música.
+    """)
+
+    
+
+st.subheader("Genera Tu Musica")
+composers = list(composer_files.keys())  
+selected_composer = st.selectbox("Selecciona un compositor:", composers, index=None)
+
+# Verificar si hay un compositor seleccionado
+if selected_composer:
+    st.write(f"Música generada basada en el estilo de {selected_composer}.")
+
+    # Asegurarse de que la semilla esté en session_state
+    if 'semilla' not in st.session_state:
+        st.session_state.semilla = pd.DataFrame()
+
+    # Botón para generar la semilla
+    if st.button("Generar Semilla"):
+        st.session_state.semilla = genera_semilla(selected_composer)
+        st.success('Se ha generado una Semilla correctamente', icon="✅")
+
+    # Comprobar si la semilla está vacía
+    if st.session_state.semilla.empty:
+        st.info('Genere una Semilla', icon="ℹ️")
+    else:
+        try:
+            model = load_lstm()
+            st.write("""
+                        El valor de la temperatura indica el nivel de creatividad en la generación musical. 
+                        Un valor cercano a 1 resulta en composiciones más realistas y coherentes, pero con menos
+                        originalidad. A medida que aumentas la temperatura, las composiciones se vuelven más creativas
+                        y variadas, aunque pueden alejarse un poco de la realidad musical.
+                     """)
+            
+            # Crear el select_slider para seleccionar la temperatura
+            temperatura = st.select_slider(
+                "Selecciona La Temperatura",
+                options=list(range(1, 101, 10))
+            )
+            # Botón para generar la predicción
+            bandera = False
+            if st.button('Generar predicción'):
+                with st.spinner('Generando predicciones...'):
+                    predicciones = generar_notas(model, st.session_state.semilla, temperatura)
+                    dataframe_a_midi(predicciones,'midi/prediccion.mid')
+                    midi_to_wav('midi/prediccion.mid','midi/prediccion.wav')
+                    bandera = True
+                    st.success('Predicciones generadas exitosamente', icon="🎶")
+
+            if bandera:
+                st.audio('midi/prediccion.wav', format="audio/wav")
+
+        except Exception as e:
+            st.error(f'Ocurrió un error: {e}', icon="⚠️")
+
+
+    st.markdown("""
+        --- 
+        **Página y modelo creados por**: Ariel Flores Santacruz
+        """)   
+    
+
+        
+
+    
